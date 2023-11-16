5eb703ae7bd78641d37d0399c6037ed559549559
SebastinST <mingchensebastin.goh@stengg.com>
2023-11-16 10:16:10 +0800

    add user id when creating job

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -32,6 +32,8 @@ exports.getAllJobs = catchAsyncErrors( async (req, res, next) => {
 // Create new job
 // /api/v1/job/new
 exports.createNewJob = catchAsyncErrors( async (req, res, next) => {
+    req.body.user = req.user.id;
+    
     const job = await Job.create(req.body);
 
     res.status(200).json({
--- models/jobSchema.js
+++ models/jobSchema.js
@@ -118,6 +118,11 @@ const jobSchema = new mongoose.Schema({
         type : [Object],
         // will not show this when normal user gets job posting information
         select : false
+    },
+    user : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'User',
+        required : true
     }
 });
 
